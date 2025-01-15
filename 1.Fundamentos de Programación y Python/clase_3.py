0d88f3d0fa5dd70b56f9975088eb7e5dde59ebcf
Juan Pablo Manrique <manriquejp1@gmail.com>
2025-01-14 22:00:03 -0500

    started the first two classes to review semantic and syntax

---
+++ 1.Fundamentos de Programación y Python/clase_3.py
@@ -0,0 +1,21 @@
+saludo = "Hola"
+nombre = "Juan"
+
+print(saludo, nombre)
+
+print("")
+
+print(saludo)
+print(nombre)
+
+print("")
+
+saludo_1 = "Hi"
+saludo_2 = "Hello"
+nombre_1 = "Juan"   
+nombre_2 = "Pablo"
+
+print(saludo_1)
+print(saludo_2)
+print(nombre_1)
+print(nombre_2)
