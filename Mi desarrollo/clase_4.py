0ef4f8952baa4e86f258392e1b68c84385441333
Juan Pablo Manrique <manriquejp1@gmail.com>
2025-06-24 21:25:38 -0500

    trabaje en progrmación orientada a objetos clase 23

---
+++ Mi desarrollo/clase_4.py
@@ -0,0 +1,23 @@
+name = "Carla Marcela"
+
+print(name[0])
+
+yo = """ Juan Pablo Manrique
+
+Es un emprendedor que no le gusta desistir y ama quien es.
+"""
+
+print(yo)
+
+
+nombre = "Juan Pablo"
+apellido = "Manrique"
+
+print(nombre + " " + apellido)
+
+
+print(len(nombre))
+
+print(nombre.upper())
+print(nombre.lower())
+print(nombre.strip())
