33bdde8ba9ee01045e57ffb9973f65a1fe42a09a
Juan Pablo Manrique <manriquejp1@gmail.com>
2025-08-20 20:10:38 -0500

    cree el juego de piedra papel y tijera

--- piedra_papel_tijera/main.py
+++ piedra_papel_tijera/main.py
@@ -17,69 +17,63 @@ import random
 juego = ["Piedra", "Papel", "Tijera"]
 
 print("Piedra = 0")
-print("Piedra = 1")
-print("Piedra = 2")
-
-jugador_1 = int(input("Jugador #1 ingrese su opción:"))
-# jugador_2 = input("Jugador #1 ingrese su opción: ")
-computador = int(random.randint(0,2))
-
-opcion_jugador = juego[jugador_1]
-opcion_computador = juego[computador]
+print("Papel = 1")
+print("Tijera = 2")
 
 puntos_jugador = 0 
 puntos_computador = 0
 
-print(f"La opcion del jugador es: {opcion_jugador}")
-print(f"La opcion del computador es: {opcion_computador}")
+while puntos_jugador <3 and puntos_computador <3:
 
-if puntos_jugador <3 and puntos_computador <3:
+    jugador_1 = int(input("Jugador #1 ingrese su opción:"))
+    # jugador_2 = input("Jugador #1 ingrese su opción: ")
+    computador = int(random.randint(0,2))
 
-    if jugador_1 == computador:
-        print("Es un empate")
-        print(f"Así va el puntaje, tu tienes {puntos_jugador} y el computador {puntos_computador}")
+    opcion_jugador = juego[jugador_1]
+    opcion_computador = juego[computador]
 
-    elif jugador_1 == 0 and computador == 2:
-        print("Piedra gana a tijera y ganas un punto")
-        puntos_jugador += 1
-        print(f"Así va el puntaje, tu tienes {puntos_jugador} y el computador {puntos_computador}")
+    print(f"La opcion del jugador es: {opcion_jugador}")
+    print(f"La opcion del computador es: {opcion_computador}")
 
-    elif jugador_1 == 1 and computador == 0:
-        print("Papel cubre a la piedra y ganas un punto")
-        puntos_jugador += 1
-        print(f"Así va el puntaje, tu tienes {puntos_jugador} y el computador {puntos_computador}")
+    if puntos_jugador >= puntos_computador or puntos_computador >= puntos_jugador:
+    
+        if jugador_1 == computador:
+            print("Es un empate")
+            print(f"Así va el puntaje, tu tienes {puntos_jugador} y el computador {puntos_computador}")
 
-    elif jugador_1 == 2 and computador == 1:
-        print("Tijera corta papel y ganas un punto")
-        puntos_jugador += 1
-        print(f"Así va el puntaje, tu tienes {puntos_jugador} y el computador {puntos_computador}")
+        elif jugador_1 == 0 and computador == 2:
+            print("Piedra gana a tijera y ganas un punto")
+            puntos_jugador += 1
+            print(f"Así va el puntaje, tu tienes {puntos_jugador} y el computador {puntos_computador}")
 
-    elif jugador_1 == 2 and computador == 0:
-        print("Piedra gana a tijera y gana el computador")
-        puntos_computador += 1
-        print(f"Así va el puntaje, tu tienes {puntos_jugador} y el computador {puntos_computador}")
+        elif jugador_1 == 1 and computador == 0:
+            print("Papel cubre a la piedra y ganas un punto")
+            puntos_jugador += 1
+            print(f"Así va el puntaje, tu tienes {puntos_jugador} y el computador {puntos_computador}")
 
-    elif jugador_1 == 0 and computador == 1:
-        print("Papel cubre a la piedra y gana el computador")
-        puntos_computador += 1
-        print(f"Así va el puntaje, tu tienes {puntos_jugador} y el computador {puntos_computador}")
+        elif jugador_1 == 2 and computador == 1:
+            print("Tijera corta papel y ganas un punto")
+            puntos_jugador += 1
+            print(f"Así va el puntaje, tu tienes {puntos_jugador} y el computador {puntos_computador}")
 
-    elif jugador_1 == 1 and computador == 2:
-        print("Tijera corta papel y gana el computador")
-        puntos_computador += 1
-        print(f"Así va el puntaje, tu tienes {puntos_jugador} y el computador {puntos_computador}")
+        elif jugador_1 == 2 and computador == 0:
+            print("Piedra gana a tijera y gana el computador")
+            puntos_computador += 1
+            print(f"Así va el puntaje, tu tienes {puntos_jugador} y el computador {puntos_computador}")
+
+        elif jugador_1 == 0 and computador == 1:
+            print("Papel cubre a la piedra y gana el computador")
+            puntos_computador += 1
+            print(f"Así va el puntaje, tu tienes {puntos_jugador} y el computador {puntos_computador}")
+
+        elif jugador_1 == 1 and computador == 2:
+            print("Tijera corta papel y gana el computador")
+            puntos_computador += 1
+            print(f"Así va el puntaje, tu tienes {puntos_jugador} y el computador {puntos_computador}") 
+
+    elif puntos_jugador == 3:
+        print("Gana el jugador")
+        break
 
     else:
-        print("Ingrese un valor válido")
-        print(f"Así va el puntaje, tu tienes {puntos_jugador} y el computador {puntos_computador}")
-
-elif puntos_jugador == 3:
-    print("Ganaste")
-else: 
-    print("Ganó el computador")
-
-
-
-
-
-
+        print("Gana el computador")
