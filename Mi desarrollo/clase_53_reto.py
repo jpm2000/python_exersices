6513538a0e80874914f0273746550609a5eee508
jpm2000 <manriquejp1@gmail.com>
2026-07-17 20:57:00 -0500

    trabaje con un reto de funciones protegidas, privadas y publicas, donde se desarrollo un codigo profesional para realizar y actualizar transacciones

--- Mi desarrollo/clase_53_reto.py
+++ Mi desarrollo/clase_53_reto.py
@@ -8,28 +8,75 @@ Comprender cuándo usar cada nivel de acceso es lo que permite escribir código 
 
 
 class Account:
-    def __init__(self, holder, number, balance):
+    def __init__(self, holder: str, number: int, balance: int):
+        if balance < 0:
+            raise ValueError("El saldo no puede ser negativo")
         self.holder = holder
         self.number = number
-        self.balance = balance
+        self.__balance = balance
 
-    def _actualizar_saldo(self):
+    @property
+    def balance(self) -> int:
+        return self.__balance
+
+    def _notificar_saldo(self, transaccion: int):
         print(
-            f"Este es el saldo actualizado {self.balance} para la cuenta numero {self.number} de {self.holder}"
+            f"Se realizó un movimiento en su cuenta {transaccion}. Este es el saldo actualizado {self.balance}, para la cuenta numero {self.number} de {self.holder}"
         )
 
-    def __registar_transaccion(self, transaction):
-        if transaction > 0:
-            self.balance += transaction
+    def __registro_transaccion(self, transaccion: int, tipo: str):
+        if tipo == "retiro":
+            if transaccion > self.__balance or transaccion <= 0:
+                raise ValueError(
+                    "La transacción no puede exeder el balance de la cuenta, ser 0 o un valor negativo"
+                )
+            elif transaccion <= self.__balance and transaccion > 0:
+                self.__balance -= transaccion
+                print(
+                    f"Usted acaba de enviar una transaccion de {transaccion}, desde su cuenta."
+                )
+            return self._notificar_saldo(transaccion)
+        elif tipo == "recibir":
+            if transaccion <= 0:
+                raise ValueError("La transacción no puede ser un valor negativo o 0")
+            else:
+                self.__balance += transaccion
+                print(f"Usted acaba de recibir {transaccion}, en su cuenta.")
+            return self._notificar_saldo(transaccion)
+        else:
+            raise ValueError(
+                "Seleccione un tipo de transaccion correcto entre retiro o recibir"
+            )
+
+    def public_method(self, transaccion: int, tipo: str) -> None:
+        self.__registro_transaccion(transaccion, tipo)
+
+
+class CuentasAhorros(Account):
+    def _notificar_saldo(self, transaccion: int):
         print(
-            f"Se ha generado una transacción de {transaction} en la cuenta {self.number}, el saldo actual es {self.balance}"
+            f"[Cuenta de Ahorros] movimiento {transaccion} nuevo saldo {self.balance} generando intereses"
         )
 
-    def public_method(self):
-        self.__registar_transaccion()
+    def __registro_transaccion(self, transaccion: int, tipo: str):
+        print("Intento de override")
 
 
-cuenta = Account("JP", "1", "0")
-cuenta._actualizar_saldo()
-# cuenta.__registar_transaccion(10000)
-# cuenta._actualizar_saldo()
+"""
+
+cuenta = Account("JP", 1, 0)
+# cuenta.public_method(0, "recibir")
+cuenta.public_method(100000, "recibir")
+print(cuenta.balance)
+# cuenta.public_method(400, "retiro")
+# print(cuenta.balance)
+cuenta.public_method(100000, "retiro")
+print(cuenta.balance)
+cuenta.public_method(50, "deposito")
+
+"""
+
+
+ahorros = CuentasAhorros("Juan Pablo", 2, 1000)
+ahorros.public_method(5000000, "recibir")
+ahorros.public_method(100, "recibir")
---
+++ Mi desarrollo/clase_54.py
@@ -0,0 +1,45 @@
+# 3 propiedades
+
+
+class Employee:
+    # Como estoy usando el guion bajo digo que el salario es un atributo protegido
+    def __init__(self, name, salary):
+        self.name = name
+        # Usar la información desde dentro de la clase
+        self._salary = salary
+
+    # Aca se usa property porque es de tipo getter.
+    @property
+    def salary(self):
+        return self._salary
+
+    # En este caso va a ser setter o sea que puedo agregar nueva informacion en un atributo
+    @salary.setter
+    def salary(self, new_salary):
+        if new_salary < 0:
+            raise ValueError("Salary cannot be negative")
+        self._salary = new_salary
+
+    # Eliminar los atributos
+    @salary.deleter
+    def salary(self):
+        print(f"The salary of {self.name} has been deleted")
+        del self._salary
+
+
+# Crear instancia de Employee
+employee = Employee("Ana", 5000)
+
+# Llamo directo al salario. COn getter puedo traerlo diretamente
+print(employee.salary)
+
+# Cambiar el salario
+employee.salary = 6000
+print(employee.salary)
+
+# Asignar un valor negativo
+# employee.salary = -6000
+# print(employee.salary)
+
+# Para eliminar
+del employee.salary
